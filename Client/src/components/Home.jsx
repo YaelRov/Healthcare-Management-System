5fd2751ccf4387f9a7c296c23ecaa49a4d799178
YaelRov <yael223590@gmail.com>
2024-07-02 19:06:01 +0300

    Home Page

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -16,12 +16,13 @@ export default function Home() {
   return (
     <div className="container central-container">
       <div className="profile-container">
-      <h1 className="profile-title">Welcome to Dr. Salomon's Clinic</h1>
+        <h1 className="profile-title"> Welcome to Salomon Medical Center</h1>
         <h3>
-          Welcome to our state-of-the-art clinic, where we provide comprehensive medical services tailored to your needs.
-          Our team of experienced doctors and specialists are dedicated to delivering the highest quality care in a compassionate and supportive environment.
+          At the forefront of compassionate care and medical excellence, we redefine the healthcare experience. Led by the renowned Dr. Salomon, our clinic combines cutting-edge technology with a personal touch.
+          Dr. Salomon's vision of patient-centered care has revolutionized how we approach health and wellness. With decades of expertise and a passion for innovation, our team sets new standards in medical practice.
+          We believe in building lasting relationships with our patients, understanding that each individual's journey to health is unique. Our state-of-the-art facility is designed to provide comfort and confidence at every step of your care.
+          Choose Salomon Medical Center for a healthcare experience that's truly exceptional.
         </h3>
-     
       </div>
     </div>
   );
